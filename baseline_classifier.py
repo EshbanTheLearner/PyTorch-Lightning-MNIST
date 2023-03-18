a8e06f26ce1d3cf1c263b647f37dfeeda15e1a8c
Eshban Suleman <eshban9492@gmail.com>
2023-03-18 17:13:01 +0500

    Baseline Classifier

---
+++ baseline_classifier.py
@@ -0,0 +1,32 @@
+from utils import get_data
+from collections import Counter
+
+train_loader, val_loader, test_loader = get_data()
+
+train_counter = Counter()
+for images, labels in train_loader:
+    train_counter.update(labels.tolist())
+
+print("\nTraining Label Distribution:")
+print(sorted(train_counter.items()))
+
+val_counter = Counter()
+for images, labels in val_loader:
+    val_counter.update(labels.tolist())
+
+print("\Validation Label Distribution:")
+print(sorted(val_counter.items()))
+
+test_counter = Counter()
+for images, labels in test_loader:
+    test_counter.update(labels.tolist())
+
+print("\Test Label Distribution:")
+print(sorted(test_counter.items()))
+
+majority_class = test_counter.most_common(1)[0]
+print(f"Majority Class: {majority_class[0]}")
+
+baseline_acc = majority_class[1] / sum(test_counter.values())
+print("Acuuracy when always prediciting the majority class:")
+print(f"{baseline_acc:.2f} ({baseline_acc*100:.2f}%)")
