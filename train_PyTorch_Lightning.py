f2509c255ca27dade83fec11055511da762d299f
Eshban Suleman <eshban9492@gmail.com>
2023-03-18 18:39:51 +0500

    PyTorch Lightning Training

--- train_PyTorch_Lightning.py
+++ train_PyTorch_Lightning.py
@@ -29,3 +29,36 @@ class LighningModel(L.LightningModule):
     def configure_optimizer(self):
         optimizer = torch.optim.SGD(self.parameters(), lr=self.learning_rate)
         return optimizer
+    
+if __name__ == "__main__":
+    print(f"Torch CUDA available? {torch.cuda.is_available()}")
+    
+    train_loader, val_loader, test_loader = get_data()
+    
+    pytorch_model = PyTorchMLP(num_features=784, num_classes=10)
+    lighning_model = LighningModel(model=pytorch_model, learning_rate=0.05)
+
+    trainer = L.Trainer(
+        max_epochs=10,
+        accelerator="auto",
+        devices="auto"
+    )
+
+    trainer.fit(
+        model=lighning_model,
+        train_dataloaders=train_loader,
+        val_dataloaders=val_loader
+    )
+
+    train_acc = compute_accuracy(pytorch_model, train_loader)
+    val_acc = compute_accuracy(pytorch_model, val_loader)
+    test_acc = compute_accuracy(pytorch_model, test_loader)
+
+    print(
+        f"Train Acc. {train_acc*100:.2f}%"
+        f" | Val Acc. {val_acc*100:.2f}%"
+        f" | Test Acc. {test_acc*100:.2f}%"
+    )
+
+PATH = "lightning.pt"
+torch.save(pytorch_model.state_dict(), PATH)
