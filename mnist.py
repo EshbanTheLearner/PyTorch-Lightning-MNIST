9f028343928a3e8ea4685fb0ef20fd0bb8240bae
Eshban Suleman <eshban9492@gmail.com>
2023-03-18 16:53:50 +0500

    Defined Data Loaders

--- mnist.py
+++ mnist.py
@@ -16,3 +16,21 @@ test_data = datasets.MNIST(
 )
 
 train_data, val_data = random_split(train_data, lengths=[55000, 5000])
+
+train_loader = DataLoader(
+    dataset=train_data,
+    batch_size=64,
+    shuffle=True
+)
+
+val_loader = DataLoader(
+    dataset=val_data,
+    batch_size=64,
+    shuffle=False
+)
+
+test_loader = DataLoader(
+    dataset=test_data,
+    batch_size=64,
+    shuffle=False
+)
